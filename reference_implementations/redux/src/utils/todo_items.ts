0a66021ccae60998848b9ab1b87de82222bf64a3
Scott Sirowy <scott.sirowy@looker.com>
2018-11-06 08:27:11 -0800

    More fixes.

--- reference_implementations/redux/src/utils/todo_items.ts
+++ reference_implementations/redux/src/utils/todo_items.ts
@@ -1,7 +1,7 @@
 // Initial todos to seed app with.
 export const todoItems = [
   'Setup skeleton project',
-  'Get your bearings and have looka around',
+  'Get your bearings and have look around',
   'List todos',
   'Create new todos',
   'Complete todos',
